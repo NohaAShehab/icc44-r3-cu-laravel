9a7d783417d2434c60a80842f8a789ca472eda51
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-08-28 10:36:10 +0300

    using laravel/ui

--- iti/routes/web.php
+++ iti/routes/web.php
@@ -144,3 +144,28 @@ Route::resource("tracks", TrackController::class);
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*
+ * GET|HEAD        login ................................ login › Auth\LoginController@showLoginForm
+  POST            login .................................. Auth\LoginController@login
+  POST            logout ..........    ................... logout › Auth\LoginController@logout
+////******************* password
+ *
+ * GET|HEAD        password/confirm ...... password.confirm › Auth\ConfirmPasswordController@showConfirmForm
+  POST            password/confirm........... Auth\ConfirmPasswordController@confirm
+  POST            password/email ..... password.email › Auth\ForgotPasswordController@sendResetLinkEmail
+  GET|HEAD        password/reset ........... password.request › Auth\ForgotPasswordController@showLinkRequestForm
+  POST            password/reset ............ password.update › Auth\ResetPasswordController@reset
+  GET|HEAD        password/reset/{token} ....... password.reset › Auth\ResetPasswordController@showResetForm
+
+ * GET|HEAD        register .............................. register › Auth\RegisterController@showRegistrationForm
+  POST            register ............................ Auth\RegisterController@register
+ *
+ *
+ *   GET|HEAD        home .............................................................. home › HomeController@index
+
+ * **/
